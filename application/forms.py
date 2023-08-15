b51c3f336bcb0aed464b4f5a867f581eb90d7a5a
James E. Bryer <jamesbryer098@gmail.com>
2023-08-15 14:40:37 +0100

    Added contact form, edited validators

--- application/forms.py
+++ application/forms.py
@@ -5,7 +5,7 @@ from application.models import CheckAdmin, BannedChars, User, Category, Product,
 
 
 class SignUpForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=30), BannedChars(), CheckAdmin(message="Name cannot be 'admin'.")])
     email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=30)])
     phone = StringField('Phone', validators=[DataRequired(), Length(min=2, max=30), BannedChars(), Regexp('^[0-9]*$', message='Phone number must be numeric.')])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
@@ -31,7 +31,13 @@ class AddressForm(FlaskForm):
 
 class PaymentForm(FlaskForm):
     cardholder_name = StringField('Cardholder Name', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    card_number = StringField('Card Number', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    expiry_date = StringField('Expiry Date', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    security_code = StringField('Security Code', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
+    card_number = StringField('Card Number', validators=[DataRequired(), Length(min=16, max=16), BannedChars(), Regexp('^[0-9]*$', message='Card number must be numeric.')])
+    expiry_date = StringField('Expiry Date', validators=[DataRequired(), Length(min=5, max=7), BannedChars(), Regexp('^(0[1-9]|1[0-2])\/(0[0-9]|1[0-9]|2[0-9]|3[0-9])$', message='Expiry date must be in format MM/YY.')])
+    security_code = StringField('Security Code', validators=[DataRequired(), Length(min=3, max=4), BannedChars(), Regexp('^[0-9]*$', message='Security code must be numeric.')])
     submit = SubmitField('Pay')
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=30)])
+    message = StringField('Message', validators=[DataRequired(), Length(min=2, max=300)])
+    submit = SubmitField('Send')
