18239cdef4e5b260ab25137da39b7ac4e0a82d03
James E. Bryer <jamesbryer098@gmail.com>
2023-08-15 14:40:01 +0100

    formatting

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
+
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite"
@@ -10,5 +11,6 @@ db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
 bcrypt = Bcrypt(app)
+
 
 from application import routes
