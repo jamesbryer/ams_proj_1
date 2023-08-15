38550cfb96817f07662ae071e1d6b3bc425fd5ce
James E. Bryer <jamesbryer098@gmail.com>
2023-08-15 08:43:25 +0100

    Updated form structure and added new validators

--- application/forms.py
+++ application/forms.py
@@ -1,15 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, DateField, IntegerField, PasswordField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from application.models import CheckAdmin, BannedChars, User, Category, Product
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
+from application.models import CheckAdmin, BannedChars, User, Category, Product, CheckPostcode
 
 
 class SignUpForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    email = StringField('Email', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    postcode = StringField('Postcode', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    phone = StringField('Phone', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=30)])
+    phone = StringField('Phone', validators=[DataRequired(), Length(min=2, max=30), BannedChars(), Regexp('^[0-9]*$', message='Phone number must be numeric.')])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=2, max=30), BannedChars(), EqualTo('password')])
     submit = SubmitField('Sign Up')
@@ -21,14 +19,14 @@ class SignUpForm(FlaskForm):
         
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=30)])
     submit = SubmitField('Login')
 
 class AddressForm(FlaskForm):
-    house_name_num = StringField('House Name/Number', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    street = StringField('Street', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
+    house_name_num = StringField('House Name/Number', validators=[DataRequired(), Length(min=1, max=30), BannedChars()])
+    street = StringField('Street', validators=[DataRequired(), Length(min=1, max=30), BannedChars()])
     town_city = StringField('Town/City', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    postcode = StringField('Postcode', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
+    postcode = StringField('Postcode', validators=[DataRequired(), CheckPostcode(message='Invalid postcode.')])
     submit = SubmitField('Update')
 
 class PaymentForm(FlaskForm):
