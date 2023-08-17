c870e4ad743e20fc4658023e38fe7223aa447f06
James E. Bryer <jamesbryer098@gmail.com>
2023-08-17 17:46:46 +0100

    reverted now Docker image made

--- application/__init__.py
+++ application/__init__.py
@@ -5,8 +5,8 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/flask_test"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/flask_test"
 
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
